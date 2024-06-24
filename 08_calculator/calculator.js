cc1a7d45d5ad65c752346c1424856998a393e47f
Cedeli <96904745+Cedeli@users.noreply.github.com>
2024-06-24 15:58:18 +0800

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(addend1, addend2) {
+	return addend1 + addend2;
 };
 
-const subtract = function() {
-	
+const subtract = function(minuend, subtrahend) {
+	return minuend - subtrahend;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let added = 0;
+  for (const num of array) {
+    added += num;
+  }
+  return added;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiplied = 1;
+  for (const num of array) {
+    multiplied *= num;
+  }
+  return multiplied;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return Math.pow(number, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) return 1;
+  let product = 1;
+  for (let i = 2; i <= number; i++) {
+    product = product * i
+  }
+  return product;
 };
 
 // Do not edit below this line
