891f4d9f07d0a91d5cbf6729b364d49d6d6fba1d
Cedeli <96904745+Cedeli@users.noreply.github.com>
2024-06-24 14:28:39 +0800

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) return 'ERROR';
 
+    let message = '';
+    for (let i = num; i > 0; i--) 
+    {
+        message += str;
+    }
+
+    return message;
 };
 
 // Do not edit below this line
