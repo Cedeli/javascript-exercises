61ed9bafcabf5c412ac464a2186a2af61222a6b9
Cedeli <96904745+Cedeli@users.noreply.github.com>
2024-06-27 19:50:55 +0800

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let filtered = [...text].filter(char => /[^\W_]/.test(char)).join('');
+    let reversed = filtered.split("").reverse().join("");
+    
+    if (filtered.toLowerCase().localeCompare(reversed.toLowerCase()) === 0) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
