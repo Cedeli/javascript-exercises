ca6e0909b434d15c55527973f10ff1fbfa57ec97
Cedeli <96904745+Cedeli@users.noreply.github.com>
2024-11-28 12:59:53 +0800

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(total) {
+    if (total < 1) return 0;
+    
+    let sequence = [1, 1];
+    for (let i = 2; i < total; i++) {
+        sequence.push(sequence[i - 2] + sequence[i - 1]);
+    }
+    
+    return sequence[total - 1];
 };
 
 // Do not edit below this line
