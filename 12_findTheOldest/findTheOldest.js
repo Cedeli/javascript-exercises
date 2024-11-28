72e2818dc8619e9cc3168dfb1cdcce77ca6f00e3
Cedeli <96904745+Cedeli@users.noreply.github.com>
2024-11-28 15:07:02 +0800

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort((a, b) => {
+        const first = (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+        const second = (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
+        return second - first;
+    })[0];
 };
 
 // Do not edit below this line
